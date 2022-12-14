650964896cd5d0c62c1be27cc523d16557346e81
Gaurav Agarwal <algogrit@gmail.com>
2022-12-14 15:41:36 +0530

    Removing account_id from output to avoid confusion.

--- users/outputs.tf
+++ users/outputs.tf
@@ -3,7 +3,6 @@ output "participant_creds" {
     for i in range(local.user_count):
       tomap({
         "email" = var.user_emails[i],
-        "account_id" = aws_iam_user.participants[i].unique_id,
         "access_key_id" =  aws_iam_access_key.participants[i].id,
         "secret" = aws_iam_access_key.participants[i].secret,
         "arn" = aws_iam_user.participants[i].arn,
